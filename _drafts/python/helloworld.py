ac08be8b4161020fb6fe76d63334f695fc7a03b0
kang <694206216@qq.com>
2019-10-26 01:12:36 +0800

    add the python snip

---
+++ _drafts/python/helloworld.py
@@ -0,0 +1,27 @@
+message = "HELLO python!"
+print(message)
+print(message.title())
+message2 = "NO python!"
+print(message)
+print(message.title())
+message3 = 'Good morning "Python",There is no country for old man!'
+print(message3)
+print(message.title())
+
+print(message + '\t' + message2 + '\t' + message3)
+
+# delete the white char on the string
+
+string = '\tgood morning '
+print(string)
+print(string.lstrip())
+print(string.rstrip())
+
+# number operate
+print(0.28 *2 + 0.1)
+
+# divide operate. 
+
+# A int number divided by a int, the result is float. This is 
+print(3/2)
+print(1/3)
