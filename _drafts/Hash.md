ade5e202dd39e8bd85fbe9026110e4263d6eba9c
kangshan <694206216@qq.com>
2020-05-22 18:32:22 +0800

    add note of hash

--- _drafts/Hash.md
+++ _drafts/Hash.md
@@ -1,12 +1,16 @@
 ---
-title: java 集合框架之 Map
+title: Hash
 date: 2017-04-13 15:02:43
 categories: programming
 tags: [programming,java, HashMap]
-description: java 集合框架中的 Map
+description: hash function && hash table
 ---
 
-# Map
+# Hash
+
+Hash 算法应用在 Java 集合框架。其中 HashTable 基本实现数据结构的 HashTable 。HashMap 的底层有 HashTable 同时也有红黑树。HashSet 的内部就是一个HashMap 。
+
+## Map
 
 - map接口：是一种将键对象和值对象进行关联的容器，而且值对象可以是另一个Map，这样类推下去可以形成多级映射；
 - Map中键对象不允许重复，且键的唯一性很重要；
@@ -16,7 +20,9 @@ description: java 集合框架中的 Map
     - TreeMap，其键按序存放
     - HashTable，是 Dictionary 的子类，与 HashMap 类似；
 
-## HashMap
+### HashMap
+
+[reference](https://www.jianshu.com/p/c658df4f4c77)
 
 - HashMap 中，其 hash 算法是在引用 Object 的 hash 方法后再对进行了额外的位运算：
 
@@ -44,8 +50,22 @@ static final int hash(Object key) {
 ```
 
 - 如此做法：将高 16 位的 hashCode 与 hashCode 低 16 位进行 XOR (异或) 位运算。这样能保证结果高 16 位为 0 ，而低 16 位还不明白是如何保证 hashCode 不会冲突的。
+- HashTable 中直接对 HashCode 按 capacity 取模，而 HashMap 中其 threshold 并不单指总 Entry 数量阈值，在其初始化时指下一次 resize() capacity 的目标值。
+- HashTable 严格按时 HashTable 数据结构来定义，使用 Object hashCode() 计算其 hash 值，并按桶数量取模放置各个值入 HashTable 中。而在 HashMap 中，其对 Key 的 hash 值进入了高 16 位与低 16 位的或运算作为最终 hash 值，同时，在放置 Node 时，也并不是直接取模，使用 `hash & (capacity - 1)` 得到 bucket 下标。
+    - 之所以能使用此种位运算获取到 bucket 下标，因为其桶数量 capacity 始终是 2 的次冥 （`10000` 的形式），`(capacity - 1)` 就会是 `1111` 的形式，而其与 hash 值进行 `&` 运算就刚好得到 hash 的模。
+    - 为了保持其 capacity 为 2 的多次冥的形式，HashMap 中使用方法专门做些事：
 
-### HashTable 与 HashMap 不同之处
-
-- hashtable 中直接对 hashcode 按 capacity 取模，而 hashMap 中其 threshold 并不单指总 Entry 数量阈值，在其初始化时指下一次 resize() capacity 的目标值。
-- HashTable 严格按时 HashTable 数据结构来定义，使用 Object hashCode() 计算其 hash 值，并按桶数量取模放置各个值入 HashTable 中。而在 HashMap 中，其对 Key 的 hash 值进入了高 16 位与低 16 位的或运算作为最终 hash 值，同时，在放置 Node 时，也并不是直接取模
+    ```java
+    /**
+     * Returns a power of two size for the given target capacity.
+     */
+    static final int tableSizeFor(int cap) {
+        int n = cap - 1;
+        n |= n >>> 1;
+        n |= n >>> 2;
+        n |= n >>> 4;
+        n |= n >>> 8;
+        n |= n >>> 16;
+        return (n < 0) ? 1 : (n >= MAXIMUM_CAPACITY) ? MAXIMUM_CAPACITY : n + 1;
+    }
+    ```
--- _drafts/invokedynamic/Test.java
+++ _drafts/invokedynamic/Test.java
@@ -1,9 +1,85 @@
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 
 public class Test{
     public static void main(String[] args){
-        String[] strs = new String[]{"adg", "adfsdf"};
-        Arrays.stream(strs).forEach(System.out::println);;
-        System.out.println("Hello, world!");
+        // String[] strs = new String[]{"adg", "adfsdf"};
+        // Arrays.stream(strs).forEach(System.out::println);
+
+        Node<String, String> loHead = null, loTail = null;
+        Node<String, String> hiHead = null, hiTail = null,
+        e = new Node<String,String>(5, "key5", "", new Node<>(9, "key9", "", 
+        new Node<>(13, "key13", "", new Node<>(17, "key17", "", null))));
+        Node<String, String> next;
+        
+        do {
+            next = e.next;
+            if ((e.hash & 4) == 0) {
+                if (loTail == null)
+                    loHead = e;
+                else
+                    loTail.next = e;
+                loTail = e;
+            } else {
+                if (hiTail == null)
+                    hiHead = e;
+                else
+                    hiTail.next = e;
+                hiTail = e;
+            }
+        } while ((e = next) != null);
+        loTail.next = null;
+        hiTail.next = null;
+        System.out.println(loHead);
+        System.out.println(hiHead);
+    }
+
+    static class Node<K, V> implements Map.Entry<K, V> {
+        final int hash;
+        final K key;
+        V value;
+        Node<K, V> next;
+
+        Node(int hash, K key, V value, Node<K, V> next) {
+            this.hash = hash;
+            this.key = key;
+            this.value = value;
+            this.next = next;
+        }
+
+        public final K getKey() {
+            return key;
+        }
+
+        public final V getValue() {
+            return value;
+        }
+
+        public final String toString() {
+            return key + "=" + value + ";next:" + next;
+        }
+
+        public final int hashCode() {
+            return Objects.hashCode(key) ^ Objects.hashCode(value);
+        }
+
+        public final V setValue(V newValue) {
+            V oldValue = value;
+            value = newValue;
+            return oldValue;
+        }
+
+        public final boolean equals(Object o) {
+            if (o == this)
+                return true;
+            if (o instanceof Map.Entry) {
+                Map.Entry<?, ?> e = (Map.Entry<?, ?>) o;
+                if (Objects.equals(key, e.getKey()) && Objects.equals(value, e.getValue()))
+                    return true;
+            }
+            return false;
+        }
     }
 }
