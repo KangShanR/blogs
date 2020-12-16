f9c2108aad2b49b672dbaa2c65ae1f3e7c74a30c
kangshan <kangshan@sm.vvip-u.com>
2020-12-16 11:16:13 +0800

    add until loop test shell

--- _drafts/shell/until_loop.sh
+++ _drafts/shell/until_loop.sh
@@ -1,8 +1,26 @@
 #!/bin/bash
-
-i=100
-until [ $i -lt 1 ]
+echo "start"
+i=0
+until [ $i -gt 10 ]
 do
-    echo -n "$i "
-    i=`expr $i - 1`
+    for j in 9 7 5
+    do
+        if [ $j -eq $i ]
+        then
+            echo "$i is equals $j"
+            continue
+        elif [ $i -lt 4 -a $i -gt 2 ]
+        then
+            echo "It's 3!"
+            break
+        elif [ $i -eq 8 ]
+        then
+            echo "It's 8,let's break 1"
+            break 1 
+        else
+            echo -n "$j" 
+        fi
+    done
+    i=`expr $i + 1`
 done
+echo "end"
